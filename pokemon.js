ffa5bcfd95306075e48fcbecd74c115a7664df39
Kri'Shawn Brown <krishawn.brown@gmail.com>
2021-02-08 20:58:49 -0500

    pokedex app

---
+++ pokemon.js
@@ -0,0 +1,15 @@
+/*const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: __dirname + '/uploads/images' });
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static());
+app.post('/upload', upload.single('photo'), (req, res) => {
+
+});
+app.listen(PORT, () => {
+    console.log('Listeniing at' + PORT);
+}); */
--- rest/index.js
+++ rest/index.js
@@ -1,11 +1,37 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.get("https://pokeapi.co/api/v2", (req, res) => {
+    res.send()
+});
+
+fetch(url)
+    .then(res => {
+
+    })
+    .catch(error => {
+
+    });
+
+fetch("https://pokeapi.co/api/v2")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    });
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Pokedex app listening at http://localhost:${port}`)
+});
+
+function myFunction() {
+    document.getElementById('btn').addEventListener("click", myFunction());
+}
+
+
+const localhost = "http://localhost:3000";
